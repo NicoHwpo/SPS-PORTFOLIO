17be1000522dd99e2b26e664532a7048b5e6155f
Nicolas <nico03aguirre@gmail.com>
2022-06-22 19:18:36 +0000

    add transition to header, sections, colors, servlet

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -26,3 +26,31 @@ function addRandomGreeting() {
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
 }
+
+
+async function showServerTime() {
+    const responseFromServer = await fetch('/date');
+    const textFromResponse = await responseFromServer.text();
+  
+    const dateContainer = document.getElementById('date-container');
+    dateContainer.innerText = textFromResponse;
+  }
+  
+
+window.addEventListener("scroll", function(){
+    const logoImage = document.querySelector(".logo img");
+    const mainNav = this.document.getElementById("mainNav");
+
+    if(window.pageYOffset > 0){
+        logoImage.style.height ="64px";
+        mainNav.classList.add("bg-black");
+        mainNav.classList.add("txt-white");
+
+    }
+    else{
+        logoImage.style.height = "84px";
+        mainNav.classList.remove("bg-black");
+        mainNav.classList.remove("txt-white");
+    }
+
+})
